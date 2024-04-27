5e8ee5df6367b531f8802f06af5349d2b686a5ef
Tushar Kaushik <tusharks2002@gmail.com>
2024-04-28 00:10:17 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("server is running on the port 3000");
+    console.log("http://localhost:3000/");
+}
+);
