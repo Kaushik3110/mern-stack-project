03a1e445f91e4448c22f813ed662b456a2274281
Tushar Kaushik <tusharks2002@gmail.com>
2024-05-01 15:16:14 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{ //each time the address starts with /api it will take to this proxy address
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
